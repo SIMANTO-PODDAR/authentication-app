95c2b6589046dff8f269577f30b1948dae4b7fac
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-27 19:47:51 +0600

    add Home btn to err & 404 page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,10 +1,14 @@
 "use client"
 
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
 const error = () => {
     return (
         <div>
             <div className="mt-20">
                 <h1 className="text-center font-bold text-2xl">Page Not Found ERROR! </h1>
+                <Link href='/' className="flex mt-5 justify-center"><Button>Home</Button></Link>
             </div>
         </div>
     );
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { Button } from "@heroui/react";
+import Link from "next/link";
+
 const notFound = () => {
     return (
         <div>
             <div className="mt-20">
                 <h1 className="text-center font-bold text-2xl">Page Not Found 404!</h1>
             </div>
+            <Link href='/' className="flex mt-5 justify-center"><Button>Home</Button></Link>
         </div>
     );
 };
