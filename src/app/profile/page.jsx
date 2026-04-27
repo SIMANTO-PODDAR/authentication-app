20877c6f33fb48832103c5cec56682c5617edd69
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-04-27 18:39:11 +0600

    update user email

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -9,7 +9,6 @@ const ProfilePage = () => {
     } = authClient.useSession()
 
     const user = session?.user;
-    const userEmail = session?.user.email.toLowerCase();
 
     if (!user) {
         return (
@@ -23,7 +22,7 @@ const ProfilePage = () => {
         <div className='grid justify-center text-left mt-3 font-bold text-xl'>
             <h1 className='text-center text-2xl my-2 underline'>Profile</h1>
             <h1>Name:  <span>{user.name.toUpperCase()}</span> </h1>
-            <h1>Email: <span className='italic'>{userEmail}</span> </h1>
+            <h1>Email: <span className='italic'>{user.email.toLowerCase()}</span> </h1>
         </div>
     );
 };
